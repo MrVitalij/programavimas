f264e71ef2cb45826ee5eb7c32b00111ba145c26
MrVitalij <vitalijjancura@gmail.com>
2023-04-07 09:12:45 +0300

    uzduotis20

--- uzduotis20/Program.cs
+++ uzduotis20/Program.cs
@@ -3,9 +3,16 @@
 namespace uzduotis20
 {
     internal class Program
+
+        // Pitagoro teorema
     {
         static void Main(string[] args)
-        {
+        
+        { 
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        
             Console.WriteLine("Įveskite pirmojo statinio ilgį:");
             double a = Convert.ToDouble(Console.ReadLine());
 
@@ -15,6 +22,8 @@ namespace uzduotis20
             double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
 
             Console.WriteLine($"Įžambinės ilgis yra: {c}");
+            Console.WriteLine();
+
         }
     }
 }
