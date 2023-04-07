645dfdc3b6660f456329dd09462a514e60e842eb
MrVitalij <vitalijjancura@gmail.com>
2023-04-07 09:06:37 +0300

    uzduotis20

---
+++ uzduotis20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uzduotis20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite pirmojo statinio ilgį:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Įveskite antrojo statinio ilgį:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            Console.WriteLine($"Įžambinės ilgis yra: {c}");
+        }
+    }
+}
