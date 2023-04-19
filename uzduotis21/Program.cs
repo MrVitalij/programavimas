5f18bace60ca1d76f488722ca732eed1c9ee7772
MrVitalij <vitalijjancura@gmail.com>
2023-04-19 08:56:35 +0300

    uzduotis21

---
+++ uzduotis21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduotis21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Masyvo kūrimas su duomenimis
+            int[] studentuPazymiai = { 10, 9, 8, 7, 6 };
+
+            // Masyvo kūrimas nusirodant dydį ir priskiriant reikšmes
+            string[] masinuMarkes = new string[3];
+            masinuMarkes[0] = "Audi";
+            masinuMarkes[1] = "BMW";
+            masinuMarkes[2] = "Mercedes";
+
+            // Duomenų išvedimas
+            Console.WriteLine("Studentų pažymiai:");
+            for (int i = 0; i < studentuPazymiai.Length; i++)
+            {
+                Console.WriteLine("Studentas Nr. " + (i + 1) + ": " + studentuPazymiai[i]);
+            }
+
+            Console.WriteLine("\nMašinų markės:");
+            for (int i = 0; i < masinuMarkes.Length; i++)
+            {
+                Console.WriteLine("Mašina Nr. " + (i + 1) + ": " + masinuMarkes[i]);
+            }
+        }
+    }
+}
