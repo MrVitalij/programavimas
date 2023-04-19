378197572f5c5354198204cea98879c12d014765
MrVitalij <vitalijjancura@gmail.com>
2023-04-19 09:09:01 +0300

    uzduotis21

--- uzduotis21/Program.cs
+++ uzduotis21/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace uzduotis21
 {
@@ -33,4 +29,5 @@ namespace uzduotis21
             }
         }
     }
+        
 }
